753f33f18714c7374c9874d0d83631b24b0664a2
misia <monika.flisowska@gmail.com>
2018-07-25 16:44:33 +0100

    css loading yaaaaassss

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
+
+const router = require('./router')
+
 const port = 3000
 
-function mainHandler (request, response) {
-    fs.readFile(path.join(__dirname, 'public','index.html'), function(error, file) {
-      if (error) {
-        console.log(error);
-        response.writeHead(500, 'Content-Type: text/html');
-        response.end('<h1>Ooops! Nothing here</h1>');
-      } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.end(file);
-      }
-    })
-}
+const server = http.createServer(router)
 
-const server = http.createServer(mainHandler)
+server.listen(port)
 
-server.listen(port, () => {
-  console.log(`server is listening on ${port}`)
-})
+console.log(`server is listening on ${port}`)
+
