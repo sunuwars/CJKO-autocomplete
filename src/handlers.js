01770c5bc8a3083505f2702399319e64c691fe36
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-07-26 14:18:40 +0100

    pinged inside handler function

--- src/handlers.js
+++ src/handlers.js
@@ -1,52 +1,58 @@
-const fs = require('fs')
-const path = require('path')
-const json = require('./obj.json')
+const fs = require("fs");
+const path = require("path");
+const json = require("./obj.json");
 
-function mainHandler (request, response) {
-    fs.readFile(path.join(__dirname, '..', 'public','index.html'), function(error, file) {
-      if (error) {
-        console.log(error);
-        response.writeHead(500, 'Content-Type: text/html');
-        response.end('<h1>Ooops! Nothing here</h1>');
-      } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
+function mainHandler(request, response) {
+  fs.readFile(path.join(__dirname, "..", "public", "index.html"), function(
+    error,
+    file
+  ) {
+    if (error) {
+      console.log(error);
+      response.writeHead(500, "Content-Type: text/html");
+      response.end("<h1>Ooops! Nothing here</h1>");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/html" });
       response.end(file);
-      }
-    })
+    }
+  });
 }
 
-function handlerPublic (request, response, url) {
-    // const loc = path.join(__dirname, '..', url);
-    // console.log({loc});
-  const extension = url.split('.')[1];
+function handlerPublic(request, response, url) {
+  // const loc = path.join(__dirname, '..', url);
+  // console.log({loc});
+  const extension = url.split(".")[1];
   const extensionType = {
-    "html": "text/html",
-    "css": "text/css",
-    "js": "application/javascript"
-  }
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript"
+  };
 
-  fs.readFile(path.join(__dirname, '..',  url), function(error, file) {
+  fs.readFile(path.join(__dirname, "..", url), function(error, file) {
     // const loc = path.join(__dirname, '..',  url);
     // console.log({loc});
     if (error) {
       console.log(error);
-      response.writeHead(500, 'Content-Type: text/html');
-      response.end('<h1>Ooops! Nothing here</h1>');
+      response.writeHead(500, "Content-Type: text/html");
+      response.end("<h1>Ooops! Nothing here</h1>");
     } else {
       response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
       response.end(file);
     }
-  })
+  });
 }
 
 function dinoHandler(request, response, url) {
-console.log(url);
+  console.log("URL=" + url);
+  var search = url.split("=")[1];
+
+  console.log(search);
 }
 
 // console.log(path.join(__dirname, '/public/', url));
 
 module.exports = {
-    mainHandler,
-    handlerPublic,
-    dinoHandler
-}
+  mainHandler,
+  handlerPublic,
+  dinoHandler
+};
