df6213214bd59cc799cae1715836f92d8ff4e0fc
misia <monika.flisowska@gmail.com>
2018-07-26 17:08:01 +0100

    suggestion list appears on the dom

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 var input = document.getElementById("input");
+var listContainer = document.getElementById("list-container");
 
 function requestData(url, cb) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status == 200) {
         data = JSON.parse(xhr.responseText);
-        console.log(data,);
         cb(data)
         return data;
       }
@@ -16,10 +16,23 @@ function requestData(url, cb) {
 
 input.addEventListener("keyup", function() {
     var userInput = document.getElementById("input").value;
-    requestData("/autocomplete/?search=" + userInput, logger)
+    requestData("/autocomplete/?search=" + userInput, populate)
 });
 
-function logger(data){
-    console.log("working", data)
-    
+function populate(data){
+    clearList();
+    var list = document.createElement("ul");
+    listContainer.appendChild(list);
+    for (var i = 0; i < 10; i++) {
+        var listItem = document.createElement("li");
+        list.appendChild(listItem);
+        var listItemContent = document.createTextNode(data[i]);
+        listItem.appendChild(listItemContent);
+    }
+}
+// add function that adds classes to list so we can style them :)
+function clearList() {
+    while(listContainer.firstChild) {
+        listContainer.removeChild(listContainer.firstChild);
+    }
 }
